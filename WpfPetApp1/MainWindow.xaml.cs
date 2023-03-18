130248a8a07e2c24b838ef3ce3733f52f25d22ce
数学天才琪露诺 <948245132@qq.com>
2023-03-18 15:28:33 +0800

    双向绑定，右键显示与关闭背景，关闭自适应大小

--- WpfPetApp1/MainWindow.xaml.cs
+++ WpfPetApp1/MainWindow.xaml.cs
@@ -19,9 +19,24 @@ namespace WpfPetApp1 {
     /// </summary>
     public partial class MainWindow : Window {
 
+        TwoWayData twoWayData = new TwoWayData() { height = 90};
+
+        /// <summary>
+        /// 主窗口
+        /// </summary>
         public MainWindow() {
             InitializeComponent();
+            AddColorDicData();
+            GetTwoWayData();
             WindowMove();
+        }
+
+        /// <summary>
+        /// 双向绑定
+        /// </summary>
+        void GetTwoWayData() {
+            DataContext = twoWayData;
+            Grid.DataContext = twoWayData;
         }
 
         /// <summary>
@@ -33,5 +48,40 @@ namespace WpfPetApp1 {
                     DragMove();
             };
         }
+
+        #region 显示背景的隐藏和显示
+
+        private Dictionary<string, Brush> colorDic = new Dictionary<string, Brush>();
+
+        /// <summary>
+        /// 添加颜色字典的数据
+        /// </summary>
+        void AddColorDicData() {
+            colorDic.Add("显示背景", new SolidColorBrush(Colors.White));
+            colorDic.Add("隐藏背景", new SolidColorBrush(Colors.Transparent));
+        }
+
+        /// <summary>
+        /// 鼠标点击事件
+        /// </summary>
+        private void itemBackground_Click(object sender, RoutedEventArgs e) {
+            colorDic.TryGetValue(itemBackground.Header.ToString(), out Brush brush);
+            if(brush != null) BorderMain.Background = brush;
+            if (itemBackground.Header.ToString().Equals("显示背景")) itemBackground.Header = "隐藏背景";
+            else itemBackground.Header = "显示背景";
+        }
+
+        
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            if(itemBackground.Header.ToString().Equals("隐藏背景")) this.ResizeMode = ResizeMode.CanResizeWithGrip;
+
+        }
+
+        #endregion
+
     }
 }
---
+++ WpfPetApp1/TwoWayData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPetApp1 {
+    class TwoWayData : INotifyPropertyChanged{
+
+        private int _height;
+
+        public int height {
+            get => _height;
+            set {
+                this._height = value;
+                if (PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("_height")); //触发事件
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
